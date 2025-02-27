a584d5b86fe1fccfb5ca34cb60efcdef81df9b3c
Robert P <popovicirobert05@gmail.com>
2025-02-28 00:06:54 +0200

    Added controllers

---
+++ ParkingLot/Controllers/ParkingLotsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParkingLot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParkingLotsController : ControllerBase
+    {
+    }
+}
